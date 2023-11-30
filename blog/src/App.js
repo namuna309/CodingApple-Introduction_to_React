57df389e9e78e90c0142ced37d92dcabaf79c7a0
namuna309 <nyah309@gmail.com>
2023-11-30 17:06:59 +0900

    중요한 데이터는 변수말고 state에 담습니다

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,21 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집'; //대충 서버에서 가져온 실제 데이터
+  let [title, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);  // a -> state에 저장했던 자료, b -> state 변경도와주는 함수
+  
+  // 일반 문법
+  let num = [1, 2];
+  var a = num[0];
+  var c = num[1]
+
+  // Destructuring 문법
+  var [a, c] = [1, 2];
 
   return (
     <div className="App">
-      <div>
-        {/*  문법 1. html에 class 넣을 땐 className */}
-        <h4 className="black-nav">블로그임</h4>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
       </div>
-      {/* 문법 2. 변수를 html에 꽂아넣을 땐 {중괄호} */}
-      <h4>{post}</h4>
-      <h4 id={post}>블로그임</h4>
-      {/* 문법 3. style 넣고 싶으면  */}
-      <h4 style={{color:'red', fontSize:'16px'}}>블로그임22</h4>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p>2월17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>2월17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>2월17일 발행</p>
+      </div>
     </div>
   );
 }
