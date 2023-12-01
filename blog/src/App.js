f69e06353ee53fc661b85e54c29021f991050e9c
namuna309 <nyah309@gmail.com>
2023-12-01 14:47:21 +0900

    array, object state 변경하는 법

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,7 +6,6 @@ import { useState } from 'react';
 function App() {
 
   let [title, changeTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);  // a -> state에 저장했던 자료, b -> state 변경도와주는 함수
-  let newTitle = ['여자 코트 추천', '강남 우동맛집', '파이썬독학'];
   let [likes, up] = useState(0);
 
   // Destructuring 문법
@@ -16,8 +15,18 @@ function App() {
     <div className="App">
       <div className="black-nav">
         {/* 숙제: 버튼누르면 첫 글이 '여자코튼 추천'으로 바뀌는 기능 만들기 */}
-        <h4>ReactBlog <span onClick={() => changeTitle(newTitle)}>성별</span></h4>
+        <h4>ReactBlog</h4>
       </div>
+      <button onClick={() => {
+        let copy = [...title];
+        copy[0] = '여자 코트 추천';
+        changeTitle(copy);
+        }}>글 수정</button>
+      <button onClick={() => {
+        let sortedTitles = [...title];
+        sortedTitles.sort();
+        changeTitle(sortedTitles);
+      }}>제목 내림차순 정렬</button>
       <div className="list">
         {/* onClick={} 안엔 함수이름 또는 콜백함수를 넣어야함 */}
         {/* state 변경은 등호로 되지 않음 -> state 변경 함수 활용 */}
