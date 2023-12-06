7bebec16fec8a60c0750a5556120998a1ee4767c
namuna309 <nyah309@gmail.com>
2023-12-06 15:29:35 +0900

    Lifecycle과 useEffect 1

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,37 +1,21 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
 function Detail(props) {
 
-    let Box = styled.button`
-        padding: 20px;
-        color: grey;
-    `;
-    // let YellowBtn = styled.button`
-    //     background: yellow;
-    //     color: black;
-    //     padding: 10px;
-    // `;
-    let YellowBtn = styled.button`
-        background: ${ props => props.bg};
-        color: ${ props => props.bg == 'blue' ? 'white' : 'black' };
-        padding: 10px;
-    `;
-
 
     let {id} = useParams();
     console.log(id);
     let shoes = props.shoes.find((element => element.id == id));
-    console.log(shoes);
-    return (
-        <>
-        <div>
-            <Box>
-                <YellowBtn bg="orange">버튼임</YellowBtn>
-                <YellowBtn bg="blue">버튼임</YellowBtn>
-            </Box>
-        </div>
+    let [cnt, setCnt] = useState(0);
+    let [disp, setDisp] = useState(true);
 
+    useEffect(() => {
+        console.log('안녕');
+        setTimeout(() => setDisp(false), 3000);
+    })
+    return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
@@ -41,11 +25,22 @@ function Detail(props) {
                     <h4 className="pt-5">{shoes.title}</h4>
                     <p>{shoes.content}</p>
                     <p>{shoes.price}원</p>
-                    <button className="btn btn-danger">주문하기</button>
+                    <button className="btn btn-danger" onClick={() => {setCnt(cnt + 1);setDisp(true)}}>주문하기</button>
+                    {
+                        disp ? <YellowBox/> : null
+                    }
                 </div>
             </div>
         </div>
-        </>
+
+        
+    )
+}
+
+function YellowBox() {
+    return(
+        <div style={{width:'100px', height:'100px', backgroundColor: 'yellow'}}>
+        </div>
     )
 }
 
