bbb79f17e61a6d28aab87848811cba53e2120fbe
namuna309 <nyah309@gmail.com>
2023-12-06 15:09:10 +0900

    styled-components 쓰면 CSS 파일 없어도 되는데

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,12 +1,37 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
 
 function Detail(props) {
+
+    let Box = styled.button`
+        padding: 20px;
+        color: grey;
+    `;
+    // let YellowBtn = styled.button`
+    //     background: yellow;
+    //     color: black;
+    //     padding: 10px;
+    // `;
+    let YellowBtn = styled.button`
+        background: ${ props => props.bg};
+        color: ${ props => props.bg == 'blue' ? 'white' : 'black' };
+        padding: 10px;
+    `;
+
 
     let {id} = useParams();
     console.log(id);
     let shoes = props.shoes.find((element => element.id == id));
     console.log(shoes);
     return (
+        <>
+        <div>
+            <Box>
+                <YellowBtn bg="orange">버튼임</YellowBtn>
+                <YellowBtn bg="blue">버튼임</YellowBtn>
+            </Box>
+        </div>
+
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
@@ -20,6 +45,7 @@ function Detail(props) {
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
