c387fb887ccc7f348586959955f6e01a48eeadb2
namuna309 <nyah309@gmail.com>
2023-12-07 16:39:33 +0900

    리액트에서 서버와 통신하려면 ajax 1 + 숙제

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import axios from 'axios';
 
 // 변수 여러개를 가져오려면 import {변수1, 변수2} from 경로
 import data from './data.js';
@@ -18,10 +19,17 @@ function App() {
   function sortCard() {
     let copy = [...shoes];
     copy.sort((a, b) => {
-      if(a.title < b.title) return -1;
-      if(a.title > b.title) return 1;
-      if(a.title == b.title) return 0;
-  })
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      if (a.title == b.title) return 0;
+    })
+    setShoes(copy);
+  }
+
+  function addCard(data) {
+    let copy = [...shoes];
+    data.map((d) => copy.push(d));
+    console.log(copy);
     setShoes(copy);
   }
 
@@ -55,6 +63,8 @@ function App() {
           <>
             <div className="main-bg"></div>
             <Button onClick={() => sortCard()}>상품 재정렬</Button>
+
+            {/* 판매 상품 진열 */}
             <div className='row'>
               {
                 shoes.map(function (s) {
@@ -64,6 +74,16 @@ function App() {
                 })
               }
             </div>
+
+            {/* 더보기 버튼 */}
+            <button onClick={() => {
+              axios.get('https://codingapple1.github.io/shop/data2.json').then((items) => {
+                addCard(items.data);
+              })
+                .catch((e) => {
+                  console.log(`실패함: ${e}`);
+                });
+            }}>더보기</button>
           </>
         } />
 
@@ -74,20 +94,21 @@ function App() {
             )
           })
         } */}
-        <Route path={'/detail/:id'} element={<Detail shoes={shoes}/>} />
-        
-        <Route path="/about" element={<About/>}>
-          <Route path="member" element={<div>멤버임</div>}/>
-          <Route path="location" element={<div>장소임</div>}/>
+        <Route path={'/detail/:id'} element={<Detail shoes={shoes} />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>장소임</div>} />
         </Route>
         {/* <Route path="/about/member" element={<div>어바웃페이지임</div>} />
         <Route path="/about/location" element={<div>어바웃페이지임</div>} /> */}
-        <Route path='event' element={<Event/>}>
-          <Route path="one" element={<div>첫 주문시 양배추즙 무료 증정</div>}/>
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>}/>
+        <Route path='event' element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 무료 증정</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
         </Route>
-        <Route path="*" element={<div>없는페이지에요</div>}/>
+        <Route path="*" element={<div>없는페이지에요</div>} />
       </Routes>
+
 
 
     </div>
