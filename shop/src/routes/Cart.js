6f2bbc7de4f16ddb855b03822cb92db6a140b0b8
namuna309 <nyah309@gmail.com>
2023-12-13 17:04:15 +0900

    성능개선 2 : 재렌더링 막는 memo, useMemo

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,16 +1,29 @@
 import { Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeAge, changeName, countUp } from '../store.js';
-import { useEffect } from 'react';
+import { useEffect, useState, memo, useMemo } from 'react';
+
+let Child = memo(function() {
+    console.log('재렌더링됨');
+    return <div>자식임</div>
+})
+
+function func() {
+    for(let i = 0; i < 1e9; i++);
+    return true;
+}
 
 function Cart() {
-
+    let result = func();
+    useMemo(() => {return func()}, [state]);
     let cart_data = useSelector((state) => state.cart);
     let state = useSelector((state) => state);
     let dispatch = useDispatch();
-
+    let [count, setCount] = useState(0);
     return (
         <div>
+            <Child count={count}></Child>
+            <button onClick={()=>setCount(count + 1)}>+</button>
             <h4>{state.user.name}({state.user.age})의 장바구니</h4>
             <button onClick={() => dispatch(changeAge(10))}>버튼</button>
             <Table>
